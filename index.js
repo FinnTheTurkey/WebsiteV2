a6261a17e6ad0820e3364936500ad1484793729b
Finn <finnandshadow@gmail.com>
2022-08-30 11:39:31 -0400

    Made a few small changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Index.js
 let markdownIt = require("markdown-it");
-let options = {html : true};
-let mdfigcaption = require('markdown-it-image-figures');
-let figoptions = {figcaption : true};
+let options = { html: true };
+let mdfigcaption = require("markdown-it-image-figures");
+let figoptions = { figcaption: true };
 
 const mdLib = markdownIt(options).use(mdfigcaption, figoptions);
 
@@ -10,14 +10,15 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = (config) => {
   config.setLibrary("md", mdLib);
   config.addPlugin(syntaxHighlight);
-  config.addPassthroughCopy('src/assets');
-  config.addPassthroughCopy('src/blog/assets');
-  config.addPassthroughCopy('src/gallery/images');
-  config.addPassthroughCopy('src/gallery/assets');
+  config.addPassthroughCopy("src/assets");
+  config.addPassthroughCopy("src/blog/assets");
+  config.addPassthroughCopy("src/gallery/images");
+  config.addPassthroughCopy("src/gallery/assets");
   config.addNunjucksGlobal("year", new Date().getFullYear());
-  config.addPairedNunjucksShortcode(
-      "test", function(content) { return `{{ test("${content}") }}`; });
-  config.addPairedShortcode("purple", function(content) {
+  config.addPairedNunjucksShortcode("test", function (content) {
+    return `{{ test("${content}") }}`;
+  });
+  config.addPairedShortcode("purple", function (content) {
     return `
 <div class="section-top purple-gradient"></div>
 <div class=" webdev-area purple-gradient" style="text-align: center">
@@ -29,24 +30,24 @@ ${content}
 `;
   });
 
-  config.addPairedNunjucksShortcode("div", function(content, div) {
+  config.addPairedNunjucksShortcode("div", function (content, div) {
     return `<div class="${div.class}">
 
 ${content}
 
 </div>
 `;
-  })
+  });
 
   return {
-    dir : {
-      input : 'src',
-      data : '../_data',
-      output : 'dist',
+    dir: {
+      input: "src",
+      data: "../_data",
+      output: "dist",
     },
 
-    markdownTemplateEngine : 'njk',
-    dataTemplateEngine : 'njk',
-    htmlTemplateEngine : 'njk',
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
   };
 };
